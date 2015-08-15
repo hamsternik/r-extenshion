64672d653c0b996472a0d408f0cb20ee997bdaa0
hamsternik <hamsternik9@gmail.com>
2015-08-16 02:49:41 +0300

    Imrove main.c: create functions to work with any numeric types

--- main.c
+++ main.c
@@ -1,25 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include <time.h>
 
 #define EPSILON 0.001
 
-void newOptimizeVec(int *inp_vec, size_t n) {
-    int *new_vec = (int *)calloc(n, sizeof(int));
-    int max_elem = inp_vec[0];
-    
-    for (size_t i=0; i < n; ++i) {
-        if (max_elem <= inp_vec[i])
-                max_elem = inp_vec[i];
-        new_vec[i] = max_elem;
-    }
-
-    for (size_t i=0; i < n; ++i)
-        printf("%i, ", *(new_vec + i));
-    printf("\n");
-
-    free (new_vec);
-}
+void vec_of_max_values_int(int*, size_t);  
+void vec_of_max_values_long(long *, size_t);  
+void vec_of_max_values_float(float *, size_t);  
+void vec_of_max_values_double(double *, size_t);  
 
 int main() {
     time_t t;
@@ -39,7 +28,75 @@ int main() {
         vec[i] = rand() % 30 + 1;
         /*scanf("%i", vec + i);*/
 
-    newOptimizeVec(vec, n); 
+    vec_of_max_values_int(vec, n); 
 
     free(vec);
+}
+
+void vec_of_max_values_int(int *inp_vec, size_t n) {
+    int *new_vec = (int *)calloc(n, sizeof(int));
+    int max_elem = inp_vec[0];
+    
+    for (size_t i=0; i < n; ++i) {
+        if (max_elem <= inp_vec[i])
+                max_elem = inp_vec[i];
+        new_vec[i] = max_elem;
+    }
+
+    for (size_t i=0; i < n; ++i)
+        printf("%i, ", *(new_vec + i));
+    printf("\n");
+
+    free (new_vec);
+}
+
+void vec_of_max_values_long(long *inp_vec, size_t n) {
+    long *new_vec = (long *)calloc(n, sizeof(long));
+    long max_elem = inp_vec[0];
+    
+    for (size_t i=0; i < n; ++i) {
+        if (max_elem <= inp_vec[i])
+                max_elem = inp_vec[i];
+        new_vec[i] = max_elem;
+    }
+
+    for (size_t i=0; i < n; ++i)
+        printf("%i, ", *(new_vec + i));
+    printf("\n");
+
+    free (new_vec);
+}
+
+void vec_of_max_values_float(float *inp_vec, size_t n) {
+    float *new_vec = (float *)calloc(n, sizeof(float));
+    float max_elem = inp_vec[0];
+
+    for (size_t i=0; i < n; ++i) {
+        if ( fabs(max_elem - inp_vec[i]) <= EPSILON )
+            max_elem = inp_vec[i];
+        new_vec[i] = max_elem;
+    }
+
+    for (size_t i=0; i < n; ++i)
+        printf("%i, ", *(new_vec + i));
+    printf("\n");
+
+    free (new_vec);
+}
+
+void vec_of_max_values_double(double *inp_vec, size_t n) {
+    double *new_vec = (double *)calloc(n, sizeof(double));
+    double max_elem = inp_vec[0];
+
+    for (size_t i=0; i < n; ++i) {
+        if ( fabs(max_elem - inp_vec[i]) <= EPSILON )
+            max_elem = inp_vec[i];
+        new_vec[i] = max_elem;
+    }
+
+    for (size_t i=0; i < n; ++i)
+        printf("%i, ", *(new_vec + i));
+    printf("\n");
+
+    free (new_vec);
 }
